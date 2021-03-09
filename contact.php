e67d8346f8ece5312a42675c8bb58879423abfae
Sashfish <dormiii4@gmail.com>
2021-03-09 15:08:15 +0000

    insert works

--- contact.php
+++ contact.php
@@ -7,9 +7,20 @@ $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-$con = new PDO($dsn, $user, $pass);
-$result = $con -> query('SELECT name, number FROM contact') -> fetchAll(PDO::FETCH_ASSOC);
+$conn = new PDO($dsn, $user, $pass);
+$result = $conn -> query('SELECT name, number FROM contact') -> fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
 
+
+if (isset($_REQUEST['name']) && isset($_REQUEST['number'])) {
+	$name = $_REQUEST['name'];
+	$number = $_REQUEST['number'];
+	
+	$conn2 = new PDO($dsn, $user, $pass);
+	$sql = "INSERT INTO contact (name, number) VALUES (?,?)";
+	$stmt = $conn2 -> prepare($sql);
+	$stmt ->execute([$name, $number]);
+	$conn2 = null;
+}
 ?>
